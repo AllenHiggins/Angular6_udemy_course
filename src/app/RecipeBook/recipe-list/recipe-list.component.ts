2e5b49439e6aa52953732a5bef21f67c21fc0ed1
AllenHiggins <allenghiggins@gmail.com>
2018-06-28 18:47:49 +0100

    recipr list item data sent to recipe detail component

--- src/app/RecipeBook/recipe-list/recipe-list.component.ts
+++ src/app/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../../Models/recipe.model';
 
 @Component({
@@ -7,12 +7,12 @@ import { Recipe } from '../../../Models/recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('A Test Recipe', 'This is a simple test', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
     // tslint:disable-next-line:max-line-length
-    new Recipe('A Test Recipe', 'This is a simple test', 'http://img.bestrecipes.com.au/RCK3slSo/h300-w400-cscale/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+    new Recipe('Another Test Recipe', 'This is a simple test', 'https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg')
   ];
 
   constructor() { }
@@ -20,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelectedEl(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
